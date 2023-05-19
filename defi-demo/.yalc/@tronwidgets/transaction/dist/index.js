42599370bdb425bd9b9bf37faab25e9fb12d0ba0
tron <tron@grayxudeMacBook-Pro.local>
2023-05-19 16:52:53 +0800

    feat: add ignore

--- defi-demo/.yalc/@tronwidgets/transaction/dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./transaction.cjs.production.min.js')
-} else {
-  module.exports = require('./transaction.cjs.development.js')
-}
